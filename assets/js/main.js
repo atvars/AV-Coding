1cad955ca9baafd136b62760396f1848348f516b
atvars <avelavs@gmail.com>
2022-01-22 09:13:21 +0000

    added offers button functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,26 @@
 let date = new Date();
 let year = date.getFullYear();
 document.getElementById('footerDate').innerHTML = year;
+
+// show offers with button click
+let showOffers = document.getElementById('offerButton');
+let infoOffers = document.getElementById('offers');
+let closeOffers = document.getElementById('closeOfferButton');
+
+closeOffers.style.display = 'none';
+infoOffers.style.display = 'none';
+
+function seeOffers() {
+  infoOffers.style.display = 'block';
+  showOffers.style.display = 'none';
+  closeOffers.style.display = 'block';
+}
+showOffers.addEventListener('click', seeOffers);
+
+function hideOffers() {
+  infoOffers.style.display = 'none';
+  showOffers.style.display = 'block';
+  closeOffers.style.display = 'none';
+}
+closeOffers.addEventListener('click', hideOffers);
+
